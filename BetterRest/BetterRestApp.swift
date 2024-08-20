7325c6c35484eb1d570d8bf7bd71ae625ead8329
AsanoGenki <asanogenki0218@icloud.com>
2024-08-20 17:08:42 +0900

    Initial commit.

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Genki on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
