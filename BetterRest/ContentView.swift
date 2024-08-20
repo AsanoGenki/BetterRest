89a1d185aae51975a7ae7abd1b31d340c893b26b
AsanoGenki <asanogenki0218@icloud.com>
2024-08-20 17:42:46 +0900

    add: 基本レイアウトの構築

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("何時に起きたいですか?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("睡眠時間")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) 時間", value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("コーヒーの摂取数")
+                    .font(.headline)
+
+                Stepper("\(coffeeAmount) カップ", value: $coffeeAmount, in: 1...20)
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("計算する", action: calculateBedtime)
+            }
         }
-        .padding()
+    }
+    func calculateBedtime() {
     }
 }
 
