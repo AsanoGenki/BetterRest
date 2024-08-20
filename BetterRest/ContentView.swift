7e3e9ed2365a955a8257e631499dd27da6378595
AsanoGenki <asanogenki0218@icloud.com>
2024-08-20 17:53:10 +0900

    add: Core MLに接続し、計算処理を作成

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Genki on 8/20/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     var body: some View {
         NavigationStack {
             VStack {
@@ -31,9 +35,29 @@ struct ContentView: View {
             .toolbar {
                 Button("計算する", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "理想の就寝時刻は..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "エラー"
+            alertMessage = "申し訳ありません、就寝時刻の計算に問題が発生しました。"
+        }
+        showingAlert = true
     }
 }
 
